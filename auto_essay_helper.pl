89c41748f76f95b124ff2b9fd037fb4f66e9e380
Lucian Green <luciangreen@lucianacademy.com>
2021-12-21 13:58:58 +1100

    auto_essay_helper.pl

---
+++ auto_essay_helper.pl
@@ -0,0 +1,32 @@
+% essay helper chicago with auto-keyword selection and sheet feeding (x this needs to be done first, changing the refs in sources)
+
+% :-include('../listprologinterpreter/listprolog.pl').
+
+:-include('short_essay_helper3.1_chicago.pl').
+:-include('../Philosophy/word_frequency_count.pl').
+:-include('../listprologinterpreter/la_files.pl').
+
+% auto_essay_helper("Title",1,member).
+
+auto_essay_helper(Title,Reasons_per_paragraph,Selection_method) :-
+	%sheet_feeder(_),
+	open_file_s("../Philosophy/connectives.txt",Connectives),
+	directory_files('sources/',F),
+	delete_invisibles_etc(F,G),
+	findall(Freq,	(member(H,G),
+	string_concat("sources/",H,H1),
+	open_file_s(H1,[_,_,_,_|J]),
+	term_to_atom(J,K),string_atom(K,K1),
+	word_frequency_count(["string",K1],Freq)),Freqs0),
+	foldl(append,Freqs0,[],Freqs),
+	%foldl(intersection,Freqs,[],Freqs1),
+	sort(Freqs,Freqs1),
+	%findall(L,member([_,L],Freqs1),M),
+	findall([O,P],(member([O,P],Freqs1),not(member(P,Connectives))),%,
+	%subtract(Freqs1,Connectives,
+	Freqs2),
+	sort(Freqs2,Freqs3),
+	(Freqs3=[[_,Q],[_,R],[_,S],[_,T],[_,U]|_]->true;
+	(writeln("Not enough keywords in sources.",abort))),
+	short_essay_helper_keywords_per_paragraph(Title,[[Q],[R],[S],[T],[U]],Reasons_per_paragraph,Selection_method),!.
+	
